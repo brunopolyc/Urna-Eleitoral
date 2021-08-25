5c1645f96b8af09e5d42fb29a40d10dc77aadd08
Bruno Polycarpo <dev.brunop@gmail.com>
2021-08-25 10:55:47 -0300

    Urna-Eleitoral

---
+++ etapas.js
@@ -0,0 +1,80 @@
+let etapas =[
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero: '04444',
+                nome: 'Natália',
+                partido: 'PR - Partido Reclama',
+                fotos:[
+                    {url: '04444.jpeg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '07777',
+                nome: 'Bruno',
+                partido: 'PP - Partido Perfeito',
+                fotos:[
+                    {url: '07777.jpeg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '01',
+                nome: 'Mônica',
+                partido: 'PDC - Partido da Cachaça',
+                vice: 'Mauro',
+                fotos:[
+                    {url: '03.jpeg', legenda: 'Prefeito'},
+                    {url: '03_1.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '02',
+                nome: 'Márcia',
+                partido: 'PDD - Partido Do Drama',
+                vice: 'Luiz',
+                fotos:[
+                    {url: '02_1.jpeg', legenda: 'Prefeito'},
+                    {url: '02.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '03',
+                nome: 'Luiz',
+                partido: 'PF - Partido Falador',
+                vice: 'Márcia',
+                fotos:[
+                    {url: '02.jpeg', legenda: 'Prefeito'},
+                    {url: '02_1.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '04',
+                nome: 'Mauro',
+                partido: 'PSQ - Partido Skol Quente',
+                vice: 'Mônica',
+                fotos:[
+                    {url: '03_1.jpeg', legenda: 'Prefeito'},
+                    {url: '03.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '05',
+                nome: 'Apolinário',
+                partido: 'PDC - Partida do Chinelo',
+                vice: 'Natália',
+                fotos:[
+                    {url: '04444_2.jpeg', legenda: 'Prefeito'},
+                    {url: '04444.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
